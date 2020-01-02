b9e7b9504487babf5aac124de66e00207a351086
Drew Dowling <drewdowling37@gmail.com>
2020-01-01 20:02:01 -0800

    more sensible login/logout links

--- server/phoenix/cookbk/lib/cookbk_web/controllers/page_controller.ex
+++ server/phoenix/cookbk/lib/cookbk_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule CookbkWeb.PageController do
   use CookbkWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", user_id: get_session(conn, :user_id))
   end
 end
